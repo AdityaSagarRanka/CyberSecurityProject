6a8eae7eeef7d2168bad9f271d1364e77d4666f4
AdityaSagarRanka <39828945+AdityaSagarRanka@users.noreply.github.com>
2020-12-02 12:20:46 +0530

    final

---
+++ CyberScript.py
@@ -0,0 +1,49 @@
+print("*****===***** OPTIONS *****===*****")
+
+print("1. Vuln Scan")
+
+print("2. Health Scan")
+
+print("3. Sten")
+
+print("4. Encryption")
+
+
+
+a=input("\n Enter the option you want to do : ")
+
+if(a==1):
+
+	print("\nWelcome to encryption")
+
+	execfile('enc.py')
+
+	
+
+elif(a==2):
+
+	print("\nWelcome to encryption")
+
+	execfile('enc.py')
+
+	
+
+elif(a==3):
+
+	print("\nWelcome to encryption")
+
+	execfile('enc.py')
+
+
+
+elif(a==4):
+
+	print("\nWelcome to encryption")
+
+	execfile('steganography.py')
+
+	
+
+else:
+
+	print("\nInvalid Option\n")
