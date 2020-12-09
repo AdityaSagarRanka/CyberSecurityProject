f378ff519a8881dacb4d06e308a1b56b1b6e7e39
param949 <54432499+param949@users.noreply.github.com>
2020-12-09 00:42:23 -0500

    Updated cyber.py

--- cyber.py
+++ cyber.py
@@ -4,6 +4,8 @@ print("1. XSS")
 
 print ("2. SQLI")
 
+print ("3. Recon")
+
 
 
 a=int(input("\n Enter the option number you want to use : "))
@@ -12,7 +14,7 @@ if(a==1):
 
 	print("\nWelcome to XSS")
 
-	exec(open("./xss_scan http://www.hotelornate.com.pk").read())
+	exec(open("./xss_scan").read())
 
 	
 
@@ -20,8 +22,12 @@ elif(a==2):
 
 	print("\nWelcome to SQLInjection")
 
-	exec(open("./sqli_scan.py  http://www.hotelornate.com.pk").read())
+	exec(open("./sqli_scan.py").read())
 
+elif(a==3):
+	print("\nWelcome to Recon")
+	
+	exec(open("./runsh.py").read())
 	
 
 else:
